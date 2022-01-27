3b5a3a33b403448d3d7c593285100b1e145e2bee
vananh <vananhbui41@gmail.com>
2022-01-27 10:37:08 +0700

    Laravel basic (1)

---
+++ database/migrations/2022_01_27_013420_create_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->bigIncrements('id')->length(10);
+            $table->string('name',255);
+            $table->integer('amount')->lenght(10);
+            $table->integer('category_id')->nullable()->length(10);
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
